f3546750e525bd4d92ceb72cde104bf04bc5ce97
Alexandre <alex.soares11@gmail.com>
2022-03-16 20:37:43 -0300

    init

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,26 @@
 import { dispatch, handleEvent } from './codeMessageHandler';
-figma.showUI(__html__);
+import { customCheckTextFills } from './lintingFunctions';
+
+figma.showUI(__html__, { width: 300, height: 600 })
 
 // The following shows how messages from the UI code can be handled in the main code.
-handleEvent('createNode', () => {
-	const node = figma.createRectangle();
-	node.name = node.id;
+handleEvent('setCornerRadius', (cornerRadiusValue) => {
+	for (const node of figma.currentPage.selection) {
+		node['cornerRadius'] = parseInt(cornerRadiusValue);
+		node['cornerSmoothing'] = 0.6;
+		figma.notify('Corner radius set, Smoothing set to iOS default ' + cornerRadiusValue, { timeout: 500 })
+	}
+    figma.ui.resize(200, 200);
 
 	// This shows how the main code can send messages to the UI code.
-	dispatch('nodeCreated', node.id);
+	dispatch('cornerRadiusSet');
 });
+
+// The following shows how messages from the UI code can be handled in the main code.
+handleEvent('lintFillStyle', () => {
+    const nodesWithError = customCheckTextFills(figma.currentPage.selection, null);
+
+    dispatch('nodesWithError', nodesWithError)
+});
+// Como eu faço pra fazer uma queue da lista e um botao pra proximo e anterior?
+// Como eu checo se os valores na customCheckTextFills(node, errors) somente nos valores que eu passei?
--- src/ui.vue
+++ src/ui.vue
@@ -1,8 +1,30 @@
 <template lang="pug">
-div
-	button.button.button--primary(@click='createNode') Create a node
-	p.type.type--pos-small-normal {{message}}
+#listOfButtons
+  h1.type--pos-xlarge-bold Sup Designer!<br />Let's get some DS consistency?
+  .divider
+  ul.disclosure
+    li.disclosure__item
+      .disclosure__label.disclosure__section Corner Radius
+      .disclosure__content
+        p.type.type--pos-small-normal Click to set corner radius to 16px
+        button.button.button--secondary(v-for="corner in corners")(
+          @click="setCornerRadius(corner.value)"
+        ) {{ corner.name }}
+    li.disclosure__item
+      .disclosure__label.disclosure__section Autolayout Spacings
+      .disclosure__content
+    li.disclosure__item
+      .disclosure__label.disclosure__section Design Linter
+      .disclosure__content
+        label.sectionTitle Select layers to look for missing color style
+        button.button.button--secondary(@click="lintFillStyle()") Search and select
+        .container
+          ul
+            li(v-for="nodeWithError in nodesWithError")
+              button.button.button-secondary Select
+                label {{ nodeWithError.name }}
 </template>
+
 
 <script>
 import { dispatch, handleEvent } from "./uiMessageHandler";
@@ -15,7 +37,15 @@ import "./figma-ui/js/disclosure";
 export default {
   data() {
     return {
-      message: ""
+      corners: [
+        { value: "4", name: "XXX-Small-Card" },
+        { value: "8", name: "XX-Small-Card" },
+        { value: "12", name: "X-Small-Card" },
+        { value: "16", name: "X-Medium Card" },
+        { value: "20", name: "Medium Card" },
+        { value: "24", name: "Large Card" },
+      ],
+      nodesWithError: [],
     };
   },
   mounted() {
@@ -25,16 +55,28 @@ export default {
     window.disclosure.init();
 
     // The following shows how messages from the main code can be handled in the UI code.
-    handleEvent("nodeCreated", nodeID => {
-      this.message = `Node ${nodeID} was created!`;
+    handleEvent("nodesWithError", (errorNodes) => {
+      this.$set(this.nodesWithError, null, errorNodes);
     });
   },
   methods: {
-    createNode() {
+    setCornerRadius(cornerRadiusValue) {
       // This shows how the UI code can send messages to the main code.
-      dispatch("createNode");
-    }
-  }
+      dispatch("setCornerRadius", cornerRadiusValue);
+    },
+    lintFillStyle() {
+      // This shows how the UI code can send messages to the main code.
+      dispatch("lintFillStyle");
+    },
+    nextErrorOccurence() {
+      // This shows how the UI code can send messages to the main code.
+      dispatch("nextErrorOccurence");
+    },
+    previousErrorOccurence() {
+      // This shows how the UI code can send messages to the main code.
+      dispatch("previousErrorOccurence");
+    },
+  },
 };
 </script>
 
